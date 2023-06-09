a494e58f8e69ceef855006a7e95448565b449b97
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-06-09 20:04:21 +0900

    入力フォームに値が入っていないときの挙動を追加

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
-//フォームの値の取得
+//入力フォームでエンターを押したとき
 form.addEventListener("submit", function(event){
   event.preventDefault(); //ブラウザのリロードを中断できる
   add(); //add関数の呼び出し
 });
 
 function add() {
-  const li = document.createElement("li"); //liタグをつくる
-  li.innerText = input.value; //input.valueをとってきてinnerTextに代入
-  li.classList.add("list-group-item"); //addでクラスを追加（スタイルをあてるため）
-  ul.appendChild(li); //ulタグの子要素としてliを追加
-  input.value = ""; //空にすることで使いやすく
+  //変数 todoTextを定義
+  let todoText = input.value;
+
+  //入力フォームに値が入力されているとき（文字数が0より大きい）→入力フォームに値が入っていないときにエンターを押しても反映されなくなる
+  if(todoText.length > 0) {
+    const li = document.createElement("li"); //liタグをつくる
+    li.innerText = todoText; //input.valueをとってきてinnerTextに代入
+    li.classList.add("list-group-item"); //addでクラスを追加（スタイルをあてるため）
+    ul.appendChild(li); //ulタグの子要素としてliを追加
+    input.value = ""; //空にすることで使いやすく
+  }
 };
