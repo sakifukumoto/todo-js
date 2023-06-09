70c9651887a9563d17665720e8892de2d8a8cdaa
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-06-09 19:37:55 +0900

    フォームの値の取得

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-//コメント
-console.log("Hello");
+const form = document.getElementById("form");
+const input = document.getElementById('input');
+
+//フォームの値の取得
+form.addEventListener('submit', function(event){
+  event.preventDefault(); //ブラウザのリロードを中断できる
+  console.log(input.value);
+});
