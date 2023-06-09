81f5777434efbf3adee0e7a6378a3364354fa9f3
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-06-09 19:47:49 +0900

    タスクの追加機能

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const form = document.getElementById("form");
-const input = document.getElementById('input');
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
 
 //フォームの値の取得
-form.addEventListener('submit', function(event){
+form.addEventListener("submit", function(event){
   event.preventDefault(); //ブラウザのリロードを中断できる
-  console.log(input.value);
+  add(); //add関数の呼び出し
 });
+
+function add() {
+  const li = document.createElement("li"); //liタグをつくる
+  li.innerText = input.value; //input.valueをとってきてinnerTextに代入
+  li.classList.add("list-group-item"); //addでクラスを追加（スタイルをあてるため）
+  ul.appendChild(li); //ulタグの子要素としてliを追加
+  input.value = ""; //空にすることで使いやすく
+};
