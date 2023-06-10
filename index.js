ccb7e68363122f60ab469bd9cdf7621ba2dd84c2
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-06-10 17:52:17 +0900

    リロード時のtodoの表示

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+//ローカルストレージに保存されているデータをJSON形式で取得
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+//todosがtrueだったらliタグを追加する
+if(todos) {
+  todos.forEach(todo => {
+    add(todo);
+  });
+}
+
 //入力フォームでエンターを押したとき
 form.addEventListener("submit", function(event){
   event.preventDefault(); //ブラウザのリロードを中断できる
   add(); //add関数の呼び出し
 });
 
-function add() {
+function add(todo) {
   //変数 todoTextを定義
   let todoText = input.value;
+
+  //todoがあったときはtodoの値でliタグをつくりたいのでtodoをtodoTextに代入
+  if(todo) {
+    todoText = todo;
+  }
 
   //入力フォームに値が入力されているとき（文字数が0より大きい）→入力フォームに値が入っていないときにエンターを押しても反映されなくなる
   if(todoText.length > 0) {
